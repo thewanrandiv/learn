a59bc8021aa3d474878fcce81d2919ac27ae4aed
Thewan Randiv Perera <thevan.randiv@gmail.com>
2024-08-19 09:10:17 +0530

    done

--- main.go
+++ main.go
@@ -25,12 +25,7 @@ func main() {
 	// server starting point
 	http.ListenAndServe(":8080", nil)
 
-	fmt.Printf("The Current time is in Nano-Seconds %v\n", time.Now().Nanosecond())
-	current_page_Size := os.Getpagesize()
 
-	fmt.Printf("The Current Page Size is %v\n", current_page_Size)
-
-	fmt.Printf("The Program Exited time is in Nano-Seconds %v\n", time.Now().Nanosecond())
 
 }
 
